e65d9e94e7e934c939a86a0e15f95ebc36c35c98
Bruno Cochofel <bruno.cochofel@gmail.com>
2021-01-23 17:59:23 +0000

    feat: version argument

--- terraplanfeed/cli.py
+++ terraplanfeed/cli.py
@@ -1,9 +1,24 @@
-"""terraformfeed cli."""
+"""Main terraformfeed cli."""
+import os
+import sys
+
+import click
+from terraplanfeed import __version__
 
 
+def version_msg():
+    """Return the Terraplanfeed version, location and Python powering it."""
+    python_version = sys.version[:3]
+    location = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    message = "Terraplanfeed %(version)s from {} (Python {})"
+    return message.format(location, python_version)
+
+
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.version_option(__version__, "-v", "--version", message=version_msg())
 def main():
     """Handle arguments."""
-    print("ok")
+    pass
 
 
 if __name__ == "__main__":
