7b891a576b581ea5b383003397b29c38e3e80d74
Bruno Cochofel <bruno.cochofel@gmail.com>
2021-01-23 17:41:11 +0000

    feat: package skeleton

---
+++ terraplanfeed/__main__.py
@@ -0,0 +1,6 @@
+"""Allow terraplanfeed to be executable through `python -m terraplanfeed`."""
+from terraplanfeed.cli import main
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
---
+++ terraplanfeed/cli.py
@@ -0,0 +1,10 @@
+"""terraformfeed cli."""
+
+
+def main():
+    """Handle arguments."""
+    print("ok")
+
+
+if __name__ == "__main__":
+    main()
