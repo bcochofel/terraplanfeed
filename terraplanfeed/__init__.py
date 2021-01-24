9b8f9d6103130ef761cfb1b8419cd3789dc92a65
Bruno Cochofel <bruno.cochofel@gmail.com>
2021-01-24 11:11:04 +0000

    refactor: base skeleton

--- terraplanfeed/__init__.py
+++ terraplanfeed/__init__.py
@@ -1,2 +1,1 @@
 """Main package for terraplanfeed."""
-__version__ = "0.1.0"
--- terraplanfeed/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Allow terraplanfeed to be executable through `python -m terraplanfeed`."""
-from terraplanfeed.cli import main
-
-
-if __name__ == "__main__":  # pragma: no cover
-    main(prog_name="terraplanfeed")
